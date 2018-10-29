4b0e4de419e3bb5cdbf3b2eb0f85cf4562d8899c
Murat Yilmaz <murat.yilmaz@jamitlabs.com>
2018-10-29 22:58:53 +0100

    Add API documentation for UITableView extensions

--- Framework/Sources/Extensions/UITableViewExtension.swift
+++ Framework/Sources/Extensions/UITableViewExtension.swift
@@ -9,31 +9,35 @@
 import UIKit
 
 extension UITableView {
+    /// Returns a reusable table view cell of type `cellType` with the name of its type as reuse identifier and adds it to the table.
     public func dequeueCell<Cell: UITableViewCell>(ofType cellType: Cell.Type, for indexPath: IndexPath) -> Cell {
         return dequeueReusableCell(withIdentifier: String(describing: cellType), for: indexPath) as! Cell
     }
 
+    /// Returns a reusable header or footer view of type `viewType` with the name of its type as reuse identifier and adds it to the table.
     public func dequeueHeaderFooterView<View: UITableViewHeaderFooterView>(ofType viewType: View.Type) -> View? {
         return dequeueReusableHeaderFooterView(withIdentifier: String(describing: viewType)) as? View
     }
 
-    public func registerCell<Cell: UITableViewCell>(ofType cellClass: Cell.Type) {
-        let cellClassName = String(describing: cellClass)
+    /// Registers a nib with the name of `cellType` if it exists or registers the class of type `cellType` as reusable cell.
+    public func registerCell<Cell: UITableViewCell>(ofType cellType: Cell.Type) {
+        let cellTypeName = String(describing: cellType)
 
-        if Bundle.main.path(forResource: cellClassName, ofType: "nib") != nil {
-            register(UINib(nibName: cellClassName, bundle: .main), forCellReuseIdentifier: cellClassName)
+        if Bundle.main.path(forResource: cellTypeName, ofType: "nib") != nil {
+            register(UINib(nibName: cellTypeName, bundle: .main), forCellReuseIdentifier: cellTypeName)
         } else {
-            register(cellClass, forCellReuseIdentifier: cellClassName)
+            register(cellType, forCellReuseIdentifier: cellTypeName)
         }
     }
 
-    public func registerHeaderFooterView<View: UITableViewHeaderFooterView>(ofType viewClass: View.Type) {
-        let viewClassName = String(describing: viewClass)
+    /// Registers a nib with the name of `viewType` if it exists or registers the class of type `viewType` as reusable header footer view.
+    public func registerHeaderFooterView<View: UITableViewHeaderFooterView>(ofType viewType: View.Type) {
+        let viewTypeName = String(describing: viewType)
 
-        if Bundle.main.path(forResource: viewClassName, ofType: "nib") != nil {
-            register(UINib(nibName: viewClassName, bundle: .main), forHeaderFooterViewReuseIdentifier: viewClassName)
+        if Bundle.main.path(forResource: viewTypeName, ofType: "nib") != nil {
+            register(UINib(nibName: viewTypeName, bundle: .main), forHeaderFooterViewReuseIdentifier: viewTypeName)
         } else {
-            register(viewClass, forHeaderFooterViewReuseIdentifier: viewClassName)
+            register(viewType, forHeaderFooterViewReuseIdentifier: viewTypeName)
         }
     }
 }
