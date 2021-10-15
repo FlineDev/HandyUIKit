f4cc3c51ac35d5e98944d2971a28527e8387e17e
Cihat Gündüz <commits@cihatguenduez.de>
2021-10-15 07:05:50 +0200

    Set supported platforms in manifest explicitly

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "HandyUIKit",
+    platforms: [.iOS(.v9), .tvOS(.v9)],
     products: [
         .library(name: "HandyUIKit", targets: ["HandyUIKit"])
     ],
